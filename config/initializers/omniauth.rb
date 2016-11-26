980a69ad7845b9bb1dbe06b28bab7b871844b702
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-11-26 20:24:06 +0000

    Update environment variable names.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
            scope: 'public_profile,email',
            info_fields: 'id,email,name'
 
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'],
            image_aspect_ratio: 'square', image_size: 48, access_type: 'online',
            name: 'google'
 
-  provider :discord, ENV['DISCORD_KEY'], ENV['DISCORD_SECRET']
+  provider :discord, ENV['DISCORD_ID'], ENV['DISCORD_SECRET']
 end
 
 OmniAuth.config.on_failure = proc do |env|
