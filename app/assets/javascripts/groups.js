45da3a8f9364d25ab915beaa1e0107b7d868d6e8
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-12-30 18:30:30 +0000

    Update share buttons

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -12,6 +12,11 @@
     $('.copy-invite').click(function() {
       new Clipboard('.copy-invite');
     });
+
+    $('.share-button').click(function() {
+      window.open(this.href, '', 'left=300,top=200,menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=250,width=600');
+      return false;
+    });
   });
 
 })();
