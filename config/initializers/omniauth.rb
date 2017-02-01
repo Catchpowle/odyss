241111cfcbd5d50a576287847f01aac1e39ce29a
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2017-02-01 21:22:45 +0000

    Update group show page and image stylings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
-           scope: 'public_profile,email',
-           info_fields: 'id,email,name'
+           scope: 'public_profile,email', info_fields: 'id,email,name',
+           image_size: 'large'
 
   provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'],
-           image_aspect_ratio: 'square', image_size: 48, access_type: 'online',
+           image_aspect_ratio: 'square', image_size: 200, access_type: 'online',
            name: 'google'
 
   provider :discord, ENV['DISCORD_ID'], ENV['DISCORD_SECRET']
